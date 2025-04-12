66b006965680027782c8e55a05f6d967d5f29b99
nishantaggarwal <naggarwal@mafcarrefour.com>
2025-04-13 01:07:48 +0530

    resilience4j

--- order-service/src/main/java/com/luv2code/ecommerce/order_service/service/OrdersService.java
+++ order-service/src/main/java/com/luv2code/ecommerce/order_service/service/OrdersService.java
@@ -6,6 +6,9 @@ import com.luv2code.ecommerce.order_service.entity.OrderItem;
 import com.luv2code.ecommerce.order_service.entity.OrderStatus;
 import com.luv2code.ecommerce.order_service.entity.Orders;
 import com.luv2code.ecommerce.order_service.repository.OrdersRepository;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.weaver.ast.Or;
@@ -26,30 +29,40 @@ public class OrdersService {
     private final ModelMapper modelMapper;
     private final InventoryOpenFeignClient inventoryOpenFeignClient;
 
-    public List<OrderRequestDto> getAllOrders(){
+    public List<OrderRequestDto> getAllOrders() {
         log.info("Fetching all Orders");
         List<Orders> orders = ordersRepository.findAll();
         return orders.stream()
-                .map(order -> modelMapper.map(order,OrderRequestDto.class)).toList();
+                .map(order -> modelMapper.map(order, OrderRequestDto.class)).toList();
     }
 
-    public OrderRequestDto getOrderById(Long id){
-        log.info("Fetching order with id: {}",id);
+    public OrderRequestDto getOrderById(Long id) {
+        log.info("Fetching order with id: {}", id);
         Orders orders = ordersRepository.findById(id)
-                .orElseThrow(()-> new RuntimeException("Order not found with id: "+id));
-        return modelMapper.map(orders,OrderRequestDto.class);
+                .orElseThrow(() -> new RuntimeException("Order not found with id: " + id));
+        return modelMapper.map(orders, OrderRequestDto.class);
     }
 
+//    @Retry(name = "inventoryRetry", fallbackMethod = "createOrderFallback")
+//    @RateLimiter(name = "inventoryRateLimiter", fallbackMethod = "createOrderFallback")
+    @CircuitBreaker(name = "inventoryCircuitBreaker",fallbackMethod = "createOrderFallback")
     public OrderRequestDto createOrder(OrderRequestDto orderRequestDto) {
+        log.info("Calling the createOrder method");
         Double totalPrice = inventoryOpenFeignClient.reduceStocks(orderRequestDto);
-        Orders orders = modelMapper.map(orderRequestDto,Orders.class);
-        for (OrderItem orderItem : orders.getItems()){
+        Orders orders = modelMapper.map(orderRequestDto, Orders.class);
+        for (OrderItem orderItem : orders.getItems()) {
             orderItem.setOrder(orders);
         }
         orders.setTotalPrice(totalPrice);
         orders.setOrderStatus(OrderStatus.CONFIRMED);
         Orders savedOrder = ordersRepository.save(orders);
 
-        return modelMapper.map(savedOrder,OrderRequestDto.class);
+        return modelMapper.map(savedOrder, OrderRequestDto.class);
+    }
+
+    public OrderRequestDto createOrderFallback(OrderRequestDto orderRequestDto, Throwable throwable) {
+        log.error("Fallback Occurred due to : {}", throwable.getMessage());
+
+        return new OrderRequestDto();
     }
 }
